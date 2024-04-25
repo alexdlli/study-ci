f2e7173059c750b58fce05c7d56c3998cbe8053b
alexdlli <alexsandrojr.303@gmail.com>
2024-04-24 22:40:44 -0300

    feat: :sparkles: create a bug in test

--- src/__test__/sum.test.ts
+++ src/__test__/sum.test.ts
@@ -6,7 +6,7 @@ describe("sum function tests", () => {
   });
 
   test("adds 0 + 0 to equal 0", () => {
-    expect(sum(0, 0)).toBe(0);
+    expect(sum(0, 8)).toBe(0);
   });
 
   test("adds -1 + -2 to equal -3", () => {
